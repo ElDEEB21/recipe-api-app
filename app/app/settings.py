e21ac4cc75ee8db2d616da90d900f0057c11c69b
ElDEEB21 <omer84484@gmail.com>
2026-03-16 04:42:42 +0200

    Add user management features with authentication and API documentation
    - Implement user creation, authentication, and profile management views.
    - Add serializers for user data handling.
    - Configure REST framework and drf-spectacular for API schema and documentation.
    - Update settings and URLs to integrate new user app functionalities.

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +134,13 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Recipe API',
+    'DESCRIPTION': 'API for managing recipes, ingredients, and tags.',
+    'VERSION': '1.0.0',
+}
